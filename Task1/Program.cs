76b86b41492027404e98a0328da79a2b5df34239
Sergey <belykov1999@yandex.ru>
2024-02-12 18:32:30 +0300

    Add Home_Seminar_02:Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿System.Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 1)
+{
+    bool result1 = number % 7 == 0;
+    bool result2 = number % 23 == 0;
+    if (result1==result2)
+    {
+    Console.WriteLine("Да");
+}
+else 
+{
+    Console.WriteLine("Нет");
+}
+}
