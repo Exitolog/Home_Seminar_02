012cedf97edd38e0505b3737a2044abae72057f9
Sergey <belykov1999@yandex.ru>
2024-02-15 19:55:09 +0300

    Add Home_Seminar_02:Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите натуральное чило от 10 до 99: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >=10 && number <= 99)
+{
+    int firstNumber = number / 10;
+    int secondNumber = number % 10;
+    if (firstNumber > secondNumber)
+    {
+        Console.WriteLine(firstNumber);
+    }
+    else 
+    {
+        Console.WriteLine(secondNumber);
+    }
+}
+else 
+{
+    Console.WriteLine("Вы ввели число не из указаного диапазона");
+}
